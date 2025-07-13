98be8e67e34e4473560f29bd0d61ed9bb09f9746
Daniel <danielrotheia@gmail.com>
2025-07-13 17:16:56 -0300

    atualizações do compose e ajustes na integração com servidor ollama

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.13-alpine3.22
 
 # Upgrade system packages and install dependencies
-RUN apt-get update && apt-get upgrade -y && apt-get install -y \
-    python3 \
-    python3-pip \
-    && rm -rf /var/lib/apt/lists/*
+RUN apk add --no-cache \
+    build-base \
+    libffi-dev \
+    gcc \
+    musl-dev \
+    linux-headers
 
 # Copy the application
 COPY . /app
@@ -17,4 +19,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Run the application
 EXPOSE 5000
-CMD ["gunicorn", "-w", "-b", "0.0.0.0:5000", "main:app"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "main:app"]
--- graph/get_model.py
+++ graph/get_model.py
@@ -4,10 +4,17 @@ import os
 
 load_dotenv()
 
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", default="qwen3:1.7b")
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", default="localhost")
+
 # __loaded_models: dict = {}
 
 def get_model(model_name: str | None = None, temperature: float = 0, reasoning: bool = False) -> OllamaLLM:
-    model_name = model_name or os.getenv("OLLAMA_MODEL", default="qwen3:1.7b")
-    return OllamaLLM(model=model_name, temperature=temperature, reasoning=reasoning, num_gpu=1)
+    model_name = model_name or OLLAMA_MODEL
+    return OllamaLLM(
+        model=model_name, 
+        temperature=temperature, 
+        reasoning=reasoning, 
+        base_url=OLLAMA_HOST)
 
     
